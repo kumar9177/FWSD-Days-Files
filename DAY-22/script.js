dccd8b6652bcd274122a0d3a60534caefc32f42a
kumarswamy <kumarswamy.mekala70.com>
2023-04-13 18:30:47 +0530

    js

---
+++ DAY-22/script.js
@@ -0,0 +1,43 @@
+// console.log(document)
+
+// const headingObject = document.querySelector('h1');
+
+// headingObject.innerHTML = 'this is new '
+
+// // console.log(headingObject)
+// const headingObject= document.getElementById('heading')
+// headingObject.innerHTML="hiikumar"
+// console.log(headingObject)
+// const listObject = document.getElementById('list')
+
+// headingObject.innerHTML = 'Hello World'
+// headingObject.style='color:red'
+
+
+// const arr=['element1','element2','element3','element4','element5']
+
+// const textNode=document.createTextNode('helloworld')
+// console.log(textNode)
+
+// arr.forEach ((element) => {
+// const li=document.createElement('li')
+// const textNode=document.createTextNode(element)
+// li.appendChild(text)
+//      li.innerHTML= element
+//     listObject.appendChild(li)
+// })
+
+// const listObject = document.getElementById('list')
+// const listElement=document.getElementsByClassName('list-ele')
+// console.log(listElement)
+// console.log([1,2,3,4])
+// listElement.forEach (ele=>{
+//     console.log(ele)
+// })
+
+// const heading=document.querySelector('ul')
+// console.log(heading)
+
+const toggleTheme=()=>{
+    console.log('togglingTheme')
+}
