7bb084635aa0ae7d68ba50688b68a410e004242d
kumarswamy <kumarswamy.mekala70.com>
2023-04-28 18:41:42 +0530

    js

---
+++ DAY-30/script.js
@@ -0,0 +1,55 @@
+// localStorage.setItem('kumar','swamy')
+
+// const obj={
+//     name:"kumar",
+//     age:21,
+// };
+
+// const arr=['tom','jerry'];
+// localStorage.setItem
+
+
+// const obj = {
+//     name: 'Tom',
+//     age: 17,
+// };
+// const arr = ['Tom', 'Bob', 'Sam'];
+
+// localStorage.setItem('nameArr', JSON.stringify(arr));
+
+// const button =  document.querySelector('button');
+// const text =  document.querySelector('span');
+// const body = document.querySelector('body');
+
+// const initialise = ()=>{
+//     const currentTheme = localStorage.getItem('theme');
+//     if(currentTheme==='dark'){
+//         body.classList.add('dark');
+//         text.innerHTML ='🌙';
+//     }else{
+//         text.innerHTML ='☀️';
+//     }
+// }
+// initialise()
+
+// const changeTheme = ()=>{
+//     const currentTheme =  body.classList.contains('dark') ? 'dark' : 'light';
+//     text.innerHTML = currentTheme==='dark'?'☀️':'🌙';
+//     body.classList.toggle('dark');
+//     localStorage.setItem('theme',currentTheme==='dark'?'light':'dark');
+// }
+
+// button.addEventListener('click',changeTheme)
+
+// console.log(navigator)
+// navigator.geolocation.getCurrentPosition((position)=>{
+//     console.log(position)
+// })
+
+
+// navigator.mediaDevices.getUserMedia({video:true}.then(stream)=>
+// console.log(stream)
+// }
+ alert='kumar';
+const quert=("what is your name");
+console.log(quert);
