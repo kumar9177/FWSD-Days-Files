fa82dea74254898bbbbd899c2a4bdd54310de71f
kumarswamy <kumarswamy.mekala70.com>
2023-05-10 10:37:24 +0530

    node js

--- DAY-33/index.js
+++ DAY-33/index.js
@@ -1,1 +1,63 @@
-console.log("hello world");
+// nodejs DAY33and 34
+
+// console.log(module)
+
+// const fs = require('fs');
+
+// const text = 'this is text from kumar'
+
+// fs.writeFileSync('temp.txt',text)
+// fs.writeFile('temp.txt',text,(err)=>{
+//     if(err) console.log(err)
+//     console.log('file created successfully')
+// })
+
+// const data=fs.readFileSync('temp.txt')
+// console.log(data)
+
+
+// console.log(data.toString())
+
+
+// const math = require('./math')
+
+// console.log(math.add(1, 2,22,45,23,45,67,89,90,100))
+
+// const math=require('./math')
+
+// console.log(math)
+
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync('./new.txt',"this is kumarswamy ",(err)=>{
+//     if(err){ console.log(err)
+//     return }
+//     console.log('file created successfully')
+// })
+
+
+// const data = fs.readFileSync('./new.txt', 'binary')
+
+// console.log(data)
+
+
+// fs.readFile('./temp.txt', 'utf-8', (err,data)=>{
+// if(err) {
+// console.log(err)
+// return
+// }
+// console.log(data)
+// })
+// const data = fs.readFileSync('./temp.txt', 'utf-8')
+
+// console.log(data,toString)
+
+
+// let i=0
+//  while(i<20)
+//  console.log(i++)
+
+const aws=require('aws-sdk');
+console.log(aws)
---
+++ DAY-33/math.js
@@ -0,0 +1,15 @@
+const add = (...rest)=> rest.reduce((acc, val)=> acc + val, 0)
+const multiply = (...rest)=> rest.reduce((acc, val)=> acc * val, 1)
+
+
+// module.exports={
+//     add,
+//     multiply,
+//     something: 'else',
+//     age:22
+// }
+
+
+// console.log('hii this is nodejs module')
+// module.exports=[1,2,3]
+
